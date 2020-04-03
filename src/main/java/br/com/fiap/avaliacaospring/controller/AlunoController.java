e3516e111512983224289e2cd2e5ebfbb8caf505
tiagokamegasawa <tiago.kamegasawa93@gmail.com>
2020-04-02 23:06:45 -0300

    Swagger

--- src/main/java/br/com/fiap/avaliacaospring/controller/AlunoController.java
+++ src/main/java/br/com/fiap/avaliacaospring/controller/AlunoController.java
@@ -5,12 +5,16 @@ import br.com.fiap.avaliacaospring.dto.CreateAlunoDTO;
 import br.com.fiap.avaliacaospring.dto.CreateCartaoCreditoDTO;
 import br.com.fiap.avaliacaospring.dto.ExtratoAlunoDTO;
 import br.com.fiap.avaliacaospring.service.AlunoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
@@ -18,35 +22,54 @@ public class AlunoController {
     @Autowired
     private AlunoService alunoService;
 
+    @ApiOperation(value = "Realiza o cadastro de um aluno", consumes = "application/json", httpMethod = "POST")
+    @ApiResponse(code = 201, message = "Cadastro de aluno realizado com sucesso.")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public AlunoDTO create(@RequestBody CreateAlunoDTO createAlunoDTO) {
         return alunoService.create(createAlunoDTO);
     }
 
+    @ApiOperation(value = "Atualiza o cadastro de um aluno", consumes = "application/json", httpMethod = "PUT")
+    @ApiResponse(code = 201, message = "Atualização realizada com sucesso.")
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public AlunoDTO update(@PathVariable Integer id, @RequestBody CreateAlunoDTO createAlunoDTO) {
+    public AlunoDTO update(
+            @PathVariable Integer id,
+            @RequestBody CreateAlunoDTO createAlunoDTO) {
         return alunoService.update(id, createAlunoDTO);
     }
 
+    @ApiOperation(value = "Adiciona um cartão para o aluno", consumes = "application/json", httpMethod = "PATCH")
+    @ApiResponse(code = 200, message = "Cartão adicionado com sucesso.")
     @PatchMapping("{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public AlunoDTO update(@PathVariable Integer id, @RequestBody CreateCartaoCreditoDTO cartaoDTO) {
+    @ResponseStatus(HttpStatus.OK)
+    public AlunoDTO update(
+            @PathVariable Integer id,
+            @RequestBody CreateCartaoCreditoDTO cartaoDTO) {
         return alunoService.update(id, cartaoDTO);
     }
 
+    @ApiOperation(value = "Exclui um aluno", consumes = "application/json", httpMethod = "DELETE")
+    @ApiResponse(code = 200, message = "Aluno excluído com sucesso.")
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Integer id) {
         alunoService.delete(id);
     }
 
+    @ApiOperation(value = "Lista todos os alunos", consumes = "application/json", httpMethod = "GET")
+    @ApiResponse(code = 200, message = "Listagem de alunos realizada com sucesso.")
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<AlunoDTO> list() {
         return alunoService.list();
     }
 
+    @ApiOperation(value = "Obtém o extrato de um aluno", consumes = "application/json", httpMethod = "GET")
+    @ApiResponse(code = 200, message = "Extrato obtido com sucesso.")
     @GetMapping("{id}/extrato")
+    @ResponseStatus(HttpStatus.OK)
     public ExtratoAlunoDTO extrato(@PathVariable Integer id) {
         return this.alunoService.extrato(id);
     }
--- src/main/java/br/com/fiap/avaliacaospring/controller/TransacaoController.java
+++ src/main/java/br/com/fiap/avaliacaospring/controller/TransacaoController.java
@@ -3,12 +3,17 @@ package br.com.fiap.avaliacaospring.controller;
 import br.com.fiap.avaliacaospring.dto.CreateTransacaoDTO;
 import br.com.fiap.avaliacaospring.dto.TransacaoDTO;
 import br.com.fiap.avaliacaospring.service.TransacaoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("/transacoes")
 public class TransacaoController {
@@ -16,25 +21,38 @@ public class TransacaoController {
     @Autowired
     private TransacaoService transacaoService;
 
+    @ApiOperation(value = "Realiza o cadastro de uma transação de um determinado cartão", consumes = "application/json", httpMethod = "POST")
+    @ApiResponse(code = 201, message = "Transação criada com sucesso.")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TransacaoDTO create(@RequestBody CreateTransacaoDTO createTransacaoDTO) {
         return transacaoService.create(createTransacaoDTO);
     }
 
+    @ApiOperation(value = "Realiza atualização de uma transação", consumes = "application/json", httpMethod = "PUT")
+    @ApiResponse(code = 200, message = "Transação atualizada com sucesso.")
     @PutMapping("{id}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public TransacaoDTO update(@PathVariable Integer id, @RequestBody CreateTransacaoDTO createTransacaoDTO) {
         return transacaoService.update(id, createTransacaoDTO);
     }
 
+    @ApiOperation(value = "Exclui uma transação", consumes = "application/json", httpMethod = "DELETE")
+    @ApiResponse(code = 200, message = "Transação excluída com sucesso.")
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void delete(@PathVariable Integer id) {
         transacaoService.delete(id);
     }
 
+    @ApiOperation(value = "Lista todas as transações para um aluno", consumes = "application/json", httpMethod = "GET")
+    @ApiResponse(code = 200, message = "Lista de transações obtida com sucesso.")
     @GetMapping
-    public List<TransacaoDTO> list(@RequestParam("alunoId") Integer alunoId) {
+    @ResponseStatus(HttpStatus.OK)
+    public List<TransacaoDTO> list(
+            @RequestParam("alunoId")
+            @ApiParam("Identificador do aluno")
+                    Integer alunoId) {
         return transacaoService.list(alunoId);
     }
 
