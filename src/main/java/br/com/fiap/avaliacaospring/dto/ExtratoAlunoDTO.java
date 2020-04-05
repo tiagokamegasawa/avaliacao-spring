1bf162b283fd7d9e67c14ebe196044b740f44e7f
tiagokamegasawa <tiago.kamegasawa93@gmail.com>
2020-04-04 21:46:08 -0300

    DTO de extrato

--- src/main/java/br/com/fiap/avaliacaospring/dto/ExtratoAlunoDTO.java
+++ src/main/java/br/com/fiap/avaliacaospring/dto/ExtratoAlunoDTO.java
@@ -7,13 +7,55 @@ import java.util.stream.Collectors;
 
 public class ExtratoAlunoDTO {
 
-    private final List<CartaoCreditoDTO> cartoes;
+    private final List<ExtratoCartaoDTO> cartoes;
 
     public ExtratoAlunoDTO(List<CartaoCredito> cartoes) {
-        this.cartoes = cartoes.stream().map(CartaoCreditoDTO::new).collect(Collectors.toList());
+        this.cartoes = cartoes.stream().map(ExtratoCartaoDTO::new).collect(Collectors.toList());
     }
 
-    public List<CartaoCreditoDTO> getCartoes() {
+    public List<ExtratoCartaoDTO> getCartoes() {
         return cartoes;
+    }
+}
+
+class ExtratoCartaoDTO {
+
+    private Integer idCartao;
+
+    private String ultimosDigitos;
+
+    private List<TransacaoDTO> transacoes;
+
+    public ExtratoCartaoDTO() {
+    }
+
+    public ExtratoCartaoDTO(CartaoCredito cartaoCredito) {
+        this.idCartao = cartaoCredito.getId();
+        this.ultimosDigitos = cartaoCredito.getUltimosDigitos();
+        this.transacoes = cartaoCredito.getTransacoes().stream().map(TransacaoDTO::new).collect(Collectors.toList());
+    }
+
+    public Integer getIdCartao() {
+        return idCartao;
+    }
+
+    public void setIdCartao(Integer idCartao) {
+        this.idCartao = idCartao;
+    }
+
+    public String getUltimosDigitos() {
+        return ultimosDigitos;
+    }
+
+    public void setUltimosDigitos(String ultimosDigitos) {
+        this.ultimosDigitos = ultimosDigitos;
+    }
+
+    public List<TransacaoDTO> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<TransacaoDTO> transacoes) {
+        this.transacoes = transacoes;
     }
 }
--- src/main/java/br/com/fiap/avaliacaospring/dto/TransacaoDTO.java
+++ src/main/java/br/com/fiap/avaliacaospring/dto/TransacaoDTO.java
@@ -1,6 +1,7 @@
 package br.com.fiap.avaliacaospring.dto;
 
 import br.com.fiap.avaliacaospring.entity.Transacao;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
@@ -40,6 +41,7 @@ public class TransacaoDTO {
         this.idCartaoCredito = idCartaoCredito;
     }
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     public LocalDateTime getDataHora() {
         return dataHora;
     }
